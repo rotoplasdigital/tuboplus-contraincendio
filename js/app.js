87a14aa42a9e6f14774abb74e0c7075bf9afe58e
Jorge Arguelles <jorge.arguelles@outlook.com>
2022-05-17 12:13:39 -0500

    Tuboplus Contra Incendio 17-05-2022

---
+++ js/app.js
@@ -0,0 +1,95 @@
+console.log('app')
+// Inicializacion de la carga de contenidos html
+includeHTML();
+// Cambio de color
+jQuery(document).ready(function($){
+	/* ==============================================================
+	/* MENU HIDE / SHOW
+	/* ============================================================== */
+	var menu = document.getElementById('menu-principal')
+	var menuLogo = document.getElementById('navegacion-logo')
+	var prevScrollpos = window.pageYOffset
+	// hide / show menu scroll
+	window.onscroll = function() {
+		showHideLogo()
+		
+		if (window.pageYOffset > 0) {
+			menu.classList.add('scrolled-alternate')
+		} else {
+			menu.classList.remove('scrolled-alternate')
+		}
+
+		var currentScrollPos = window.pageYOffset;
+		if (prevScrollpos > currentScrollPos) {
+			menu.classList.remove('scrolled-down')
+			menu.classList.add('scrolled-up')
+		} else {
+			menu.classList.remove('scrolled-up')
+			menu.classList.add('scrolled-down')
+		}
+		prevScrollpos = currentScrollPos
+	}
+	window.addEventListener('resize', showHideLogo)
+	// hide show logo
+	function showHideLogo() {
+		console.log('showHideLogo')
+		//desktop
+		if(document.body.classList.contains('inicio') && window.pageYOffset > 0 && window.innerWidth > 576) {
+			console.log('inicio:offset>0:width>576')
+			menuLogo.src = '/images/rotoplas_logo_blanco_azul.svg'
+			menuLogo.classList.remove('opacity-0')
+			menuLogo.classList.add('opacity-1')
+		}
+		if(document.body.classList.contains('inicio') && window.pageYOffset <= 0 && window.innerWidth > 576) {
+			menuLogo.classList.remove('opacity-1')
+			menuLogo.classList.add('opacity-0')
+		}
+		//movil
+		if( (document.body.classList.contains('inicio')) && (window.pageYOffset <= 0) && (window.innerWidth < 576) ) {
+			console.log('inicio:offset<0:width<577')
+			menuLogo.src = '/images/rotoplas_logo.svg'
+			menuLogo.classList.remove('opacity-0')
+		}
+		if(document.body.classList.contains('inicio') && window.pageYOffset > 0 && window.innerWidth < 576) {
+			console.log('inicio:offset>0:width<576')
+			menuLogo.src = '/images/rotoplas_logo_blanco_azul.svg'
+			menuLogo.classList.remove('opacity-0')
+			menuLogo.classList.add('opacity-1')
+		}
+		// desktop internas
+		if( (document.body.classList.contains('interno')) ) {
+			console.log('interno')
+			menuLogo.src = '/images/rotoplas_logo.svg'
+			menuLogo.classList.remove('opacity-0')
+			menuLogo.classList.add('opacity-1')
+		}
+		if( (document.body.classList.contains('interno')) && (window.pageYOffset > 0) ) {
+			console.log('interno:offset>0:width>576')
+			menuLogo.src = '/images/rotoplas_logo_blanco_azul.svg'
+			menuLogo.classList.add('opacity-1')
+		}
+	}
+
+	showHideLogo()
+	/* ==============================================================
+	/* SLIDER
+	/* ============================================================== */
+	$('.carac-dif-nodo-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 577,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	})
+})
